0fa327b213a49c330dd90b82bbcfbda5289471c0
JaviCMDev <capillaj.dev@gmail.com>
2023-02-13 13:54:41 +0100

    CRUD Basico Laravel

---
+++ app/Http/Controllers/Api/V1/BookController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Book::first()->paginate(5);
+    }
+
+    public function getByTitle($value)
+    {
+        return Book::where('title', $value)->get();
+    }
+
+    public function getById($value)
+    {
+        return Book::find($value);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Book::create($request->all());
+        return 'done';
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        Book::where('id', $request->id)->update($request->all());
+        return 'update done';
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Book::where('id',$id)->delete();
+        return 'delete done';
+    }
+}
